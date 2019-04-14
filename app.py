f62b7e60d05a314eb8dd1f078f9a701dc99d9811
Narayan Manivannan <nmanivan@umich.edu>
2019-04-14 17:26:06 -0400

    testing simple case for messenger_app. Using aws ec2 as endpoint

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+import random
 from flask import Flask, request
+from pymessenger.bot import Bot
+
 
 app = Flask(__name__)
 ACCESS_TOKEN = 'ACCESS_TOKEN'
-VERIFY_TOKEN = 'VERIFY_TOKEN'
+VERIFY_TOKEN = '5941526563'
 bot = Bot(ACCESS_TOKEN)
 
 
 def verify_fb_token(token_sent):
     #take token sent by facebook and verify it matches the verify token you sent
     #if they match, allow the request, else return an error 
+    # print(token_sent)
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
@@ -33,23 +37,24 @@ def receive_message():
         # Before allowing people to message your bot, Facebook has implemented a verify token
         # that confirms all requests that your bot receives came from Facebook. 
         token_sent = request.args.get("hub.verify_token")
+        # print('hello')
         return verify_fb_token(token_sent)
-     else:
+    else:
         # get whatever message a user sent the bot
-       output = request.get_json()
-       for event in output['entry']:
-          messaging = event['messaging']
-          for message in messaging:
-            if message.get('message'):
-                #Facebook Messenger ID for user so we know where to send response back to
-                recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for message in messaging:
+                if message.get('message'):
+                    #Facebook Messenger ID for user so we know where to send response back to
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        response_sent_text = get_message()
+                        send_message(recipient_id, response_sent_text)
+                    #if user sends us a GIF, photo,video, or any other non-text item
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = get_message()
+                        send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
 
---
+++ ssh_aws_info/ssh_to_ec2.sh
@@ -0,0 +1,1 @@
+ssh -i My_instance_key4.pem ubuntu@ec2-54-173-71-180.compute-1.amazonaws.com
