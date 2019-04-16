93e3c521425ffb162051c2dd942f0f16679965ac
duquetr <duquetr@umich.edu>
2019-04-15 23:38:42 -0400

    more messages for panic affirmations

--- panic_advice.py
+++ panic_advice.py
@@ -30,14 +30,14 @@ def handle_panic_advice(req):
                 elif trigger_type == 'fight':
                     response = panic_advice_home_fight(req)
                 else:
-                    response = '''ERROR IN LOCATION'''
+                    response = "ERROR IN LOCATION"
             elif loc == 'work':
                 if trigger_type == 'health':
                     response = panic_advice_work_health(req)
                 elif trigger_type == 'fight':
                     response = panic_advice_work_fight(req)
                 else:
-                    response = '''ERROR IN LOCATION'''
+                    response = "ERROR IN LOCATION"
 
             elif loc == 'school':
                 if trigger_type == 'health':
@@ -45,10 +45,10 @@ def handle_panic_advice(req):
                 elif trigger_type == 'fight':
                     response = panic_advice_school_fight(req)
                 else:
-                    response = '''ERROR IN LOCATION'''
+                    response = "ERROR IN LOCATION"
 
             else:
-                response = '''ERROR IN LOCATION'''
+                response = "ERROR IN LOCATION"
         else:
             if trigger_type == "":
                 response = panic_advice_unknown(req)
@@ -57,42 +57,59 @@ def handle_panic_advice(req):
             elif trigger_type == 'fight':
                 response = panic_advice_fight(req)
             else:
-                response = '''ERROR''' 
-
+                response = "ERROR" 
+    print("RESPONSE IS: ", response)
     return response   
 
 def panic_advice_school_fight(req):
-    output = '''school fight'''
+    output = '''school_fight: Working with other students can be hard. \
+        Many schools have mental health support available should you decide that's necessary. \
+        There may also be faculty available if you're having problems working with others students. \
+        Be kind to yourself, and make sure you're reaching out for help with friends and students when you need it! \
+        How are you feeling now? Are you feeling better, or would you like to talk more?'''
     return output
 
 def panic_advice_work_fight(req):
-    output = '''work fight'''
+    output = '''work_fight: Professional relationships can be confusing to navigate. \
+        If your work has a human resources department, you can reach out to them for support. \
+        If not, make sure to reach out to your friends and family for support when work gets tough. \
+        Many people want to "leave work at work," which can be healthy, but we need to make sure to reach out for support when \
+            work gets really tough. \
+        How are you feeling now? Are you feeling better, or would you like to talk more?'''
     return output
 
 def panic_advice_home_fight(req):
-    output = '''home_fight'''
+    output = '''home_fight: Family problems can be difficult to navigate, especially if they're very personal. \
+        How are you feeling now? Are you feeling better, or would you like to talk more?'''
     return output
 
 def panic_advice_school_health(req):
-    output = '''school_health'''
+    output = '''school_health: Having a health problem at school can be really scary, but is one of the safer places you can be should something happen. \
+        Make sure you've told someone about this health issue if it's recurring, especially if it's actually scaring you. \
+        Having someone to support you can help a lot and take away a little bit of that fear and feeling of helplessness. \
+        How are you feeling now? Are you feeling better, or would you like to talk more?'''
     return output
 
 def panic_advice_work_health(req):
-    output = '''work_health'''
+    output = '''work_health: \
+        How are you feeling now? Are you feeling better, or would you like to talk more?'''
     return output
 
 def panic_advice_home_health(req):
-    output = '''home_health'''
+    output = '''home_health: \
+        How are you feeling now? Are you feeling better, or would you like to talk more?'''
     return output
 
 def panic_advice_health(req):
-    output = '''just health'''
+    output = '''just health: \
+        How are you feeling now? Are you feeling better, or would you like to talk more?'''
     return output
 
 def panic_advice_fight(req):
-    output = '''just fight'''
+    output = '''just fight: \
+        How are you feeling now? Are you feeling better, or would you like to talk more?'''
     return output
 
 def panic_advice_unknown(req):
-    output = '''I'm so sorry to hear that. I've never dealt with that situation, so I don't have any advice. make sure you keep breathing and reach out to someone you know if you need more help!'''
+    output = "I'm so sorry to hear that. I've never dealt with that situation, so I don't have any advice. make sure you keep breathing and reach out to someone you know if you need more help!"
     return output
